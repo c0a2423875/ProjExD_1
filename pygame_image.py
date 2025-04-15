9128d20aacd437e4eb75218c0536ba8cf59462cb
c0a2423875 <c0a2423875@edu.teu.ac.jp>
2025-04-15 15:57:27 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -20,6 +20,7 @@ def main():
 
         x = tmr
         screen.blit(bg_img, [-x, 0])  #6
+        screen.blit(bg_img, [-x+1600, 0])
         screen.blit(kk_img, [300, 200]) #4
         pg.display.update()
         tmr += 1        
